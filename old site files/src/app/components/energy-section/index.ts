0ae5bdc167c61fd8c42850d64ea6e3e01f016f5e
Amol derick soans <amoldericksoans@gmail.com>
2026-01-30 23:26:24 +0530

    removed old site

--- old site files/src/app/components/energy-section/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as EnergySection } from './EnergySection';
-export { default as EnergyHeroSection } from './EnergyHeroSection';
-export { default as AdvantagesCarousel } from './AdvantagesCarousel';
-export { default as FeatureBlocks } from './FeatureBlocks';
-export { default as EnergyCTASection } from './EnergyCTASection';
--- old site files/src/app/components/gpu-selection/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as GPUSelectionSection } from './GPUSelectionSection';
-export { default as GPUCard } from './GPUCard';
-export { default as GPUImage } from './GPUImage';
